4340f64b8175823ec0292263a9714ee72f8c0c18
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2024-05-10 09:19:22 +0300

    Form1.cs

--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
-namespace Lab7CSharp
+namespace TextBoxToRichTextBox
 {
     public partial class Form1 : Form
     {
@@ -17,9 +10,13 @@ namespace Lab7CSharp
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void addButton_Click(object sender, EventArgs e)
         {
-
+            string currentTime = DateTime.Now.ToString("HH:mm:ss");
+            string sentence = textBox.Text;
+            string textToAdd = $"[{currentTime}] {sentence}\n";
+            richTextBox.AppendText(textToAdd);
+            textBox.Clear();
         }
     }
 }
